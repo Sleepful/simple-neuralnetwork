b992b542eac5d4255b7eef0a4323c026de69f13f
Raquel <raqe-11@hotmail.com>
2018-10-17 10:45:11 -0600

    full integration between graphic and algorithm

--- ann.py
+++ ann.py
@@ -4,35 +4,35 @@ from matplotlib import pyplot as plt
 #################
 ### Variables ###
 #################
-coordLim = 20  # Define the limit of the graphic
+coordLim = 100  # Define the limit of the graphic
 plt.isinteractive()
+pointsX = []
+pointsY = []
+linePointsX = []
+linePointsY = []
 
 ####################################
 # receive the beginning and the    #
 # destiny of the line and print it #
 ####################################
-def printLine(cord1, cord2):
-    pointx = np.linspace(cord1[0], cord2[0])
-    pointy = np.linspace(cord1[1], cord2[1])
-    plt.axhline(0, color="red")
-    plt.axvline(0, color="red")
-    plt.xlim(-coordLim, coordLim)
-    plt.ylim(-coordLim, coordLim)
-    plt.plot(pointx, pointy)
-    plt.draw()
-    plt.pause(0.5)
-    plt.close()
+def printLine(cord1, cord2, lineCord1, lineCord2):
+    #pointx = np.linspace(cord1[0], cord2[0])
+    #pointy = np.linspace(cord1[1], cord2[1])
+    plt.axhline(0, color="red")    #draw a red line that divide X axis
+    plt.axvline(0, color="red")    #draw a red line that divide Y axis
 
-'''
-for i in range(10):
-    for j in range(10):
-        printLine((i, j), (i + 10, j + 10))
-'''
+    plt.xlim(-coordLim,coordLim)
+    plt.ylim(-coordLim,coordLim)
+    plt.plot(cord1, cord2, "go", lineCord1, lineCord2, "r-")
+    plt.show()      #use .draw() when time pause is enabled
+
+    #plt.pause(0.5) #tiempo de espera para que muestre la imagen
+    #plt.close()    #cierra la gráfica
 
 class TrainingGrounds:
     def __init__(self, m, b):
-        self.m          = m #entre -10 y 10
-        self.b          = b #entre 0 y 1
+        self.m          = m #between 0 y 100
+        self.b          = b #between 0 y 1
         self.generate_new_dot()
         self.classify_dot()
 
@@ -80,18 +80,34 @@ if __name__ == "__main__":
     trainer = TrainingGrounds(m,b)
     x = trainer.dot
     y = trainer.C
+
     percept = Perceptron(2)
 
     for i in range(1000):
         trainer.generate_new_dot()
+
         X = trainer.dot
         d = trainer.C
+
         percept.fit(X, d)
+
 
     for i in range(20):
         trainer.generate_new_dot()
+
         X = trainer.dot
         d = trainer.C
+
+        pointsX.append(int(X[0]))
+        pointsY.append(int(X[1]))
+
+        y = m*pointsX[i]+b      #finds the points in Y axis of the dividing line
+
+        linePointsX.append(m*int(X[0]))
+        linePointsY.append(y)
+
         p = percept.predict(X)
-        print(i, "-- predict ", p, " answer ", d)
-        #printLine(x, y)
+
+        print("Point ", pointsX[i], ",", pointsY[i], " -- predict ", p, " answer ", d)
+
+    printLine(pointsX, pointsY, linePointsX, linePointsY)   #print the graphic with all elements
--- tareaIA.py
+++ tareaIA.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 #################
 coordLim = 10 #Define the limit of the graphic
 plt.isinteractive()
+points = [(1,5), (4,-3), (7,-5)]
 
 #################
 ###  Program  ###
@@ -16,20 +17,22 @@ plt.isinteractive()
 # destiny of the line and print it #
 ####################################
 def printLine(cord1, cord2):
+        print(cord1[0], "...", cord2[0])
         pointx = np.linspace(cord1[0], cord2[0])
         pointy = np.linspace(cord1[1], cord2[1])
         plt.axhline(0, color="red")
         plt.axvline(0, color="red")
         plt.xlim(-coordLim, coordLim)
         plt.ylim(-coordLim, coordLim)
-        plt.plot(pointx, pointy)
-        plt.draw()
-        plt.pause(0.5)
-        plt.close()
-    
+        plt.plot([1,5], [5,7], 'r--', [1,4,7], [5,-3,-5], 'gD')
+        plt.show()
+        #plt.pause(0.5)
+        #plt.close()
 
+printLine((-1, 3), (5, 7))
+    
+'''
 for i in range(10):
     for j in range(10):
         printLine((i, j), (i+10, j+10))
-
-        
+'''
